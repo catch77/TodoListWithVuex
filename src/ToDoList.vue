20ca288789653f99c69161c56cca040c328921fa
catch77 <1297056296@qq.com>
2019-07-22 14:06:48 +0800

    first commit

---
+++ src/ToDoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="ToDoList">
+    <Header></Header>
+    <Body></Body>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Body from './components/Body.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'ToDoList',
+  components: {
+    Header,
+    Body,
+    Footer
+  },
+}
+</script>
+
+<style>
+
+</style>
